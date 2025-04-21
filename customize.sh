e718114cf4633b97384beb8b50438259f49aa22c
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-22 01:38:43 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+mkdir -p openwrt/files/etc/config
+echo -e "config core 'main'\n\toption lang 'en'" > openwrt/files/etc/config/luci
+
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
