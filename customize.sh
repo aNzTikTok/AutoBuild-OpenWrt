1199649c432ece87b2595039f2439e8739bb7ba8
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-22 01:40:59 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Create config directory
 mkdir -p openwrt/files/etc/config
-echo -e "config core 'main'\n\toption lang 'en'" > openwrt/files/etc/config/luci
 
+# Force LuCI interface language to English
+cat <<EOF > openwrt/files/etc/config/luci
+config core 'main'
+	option lang 'en'
+EOF
+
+# Set timezone to Malaysia (Kuala Lumpur)
+cat <<EOF > openwrt/files/etc/config/system
+config system
+	option hostname 'OpenWrt'
+	option timezone 'MST-8'
+	option zonename 'Asia/Kuala Lumpur'
+EOF
+
+# Remove default root password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
